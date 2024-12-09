545065bbc9921f63dd790afef90083ebf21cdc7a
scyyw17 <scyyw17@exmail.nottingham.ac.uk>
2024-12-09 10:00:33 +0800

    Add instruction

--- main.py
+++ main.py
@@ -4,16 +4,11 @@ from game_logic.market import Market
 from ui.GUI import StockMarketUI
 
 def main():
-    # 加载股票数据
     stock_data = StockData.load_data("data/UK_FTSE100 Index.csv")
-    
-    # 初始化市场和音频管理器
-    market = Market(initial_money=1000.0)
-    sound_manager = SoundManager("audio",stock_data['Close'][0])
-
-    # 启动用户界面
+    market = Market(initial_money=100000.0)  # Initial balance set to $100,000
+    sound_manager = SoundManager("audio", stock_data['Close'][0])
     ui = StockMarketUI(market, stock_data, sound_manager)
-    ui.create_main_window()
+    ui.show_instruction_window()
 
 if __name__ == "__main__":
     main()
